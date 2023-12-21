d6880c74a5bf0300ab188f86a8266693778a43b9
Aya <147270454+ayaarias21@users.noreply.github.com>
2023-12-20 20:21:11 -0500

    Update 2_Radicalization.py

--- pages/2_Radicalization.py
+++ pages/2_Radicalization.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Bytes and Bombs", page_icon="🚨", layout="centered")
