ce5f79a243bfbe18135f741f5a8e497c91ae7389
Aya <147270454+ayaarias21@users.noreply.github.com>
2023-12-20 20:16:23 -0500

    Update and rename 2_Mapping_Demo.py to 2_Radicalization.py

---
+++ pages/2_Radicalization.py
@@ -0,0 +1,55 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+st.set_page_config(page_title="Bytes and Bombs", page_icon="🚨", layout="centered")
+st.markdown("<h2 style='color: #990000;'>Radicalization</h2>", unsafe_allow_html=True)
+
+
+article_intro = """
+The article entitled “Rethinking Online Radicalization” by Joe Whittaker does an excellent job at challenging 
+the understanding of online radicalization by arguing against the separation between the “online” and “offline” realms. 
+The article suggests both domains are inseparable, and we should understand the ontological nature of it and by doing 
+this adopting a broader range of factors beyond online communication technologies when studying radicalization.
+"""
+
+article_main = """
+The article refers to various models and theories proposed by scholars to explain the dynamics of online radicalization, 
+emphasizing the potential for individuals to be exposed to extremist propaganda, engage in deviant behaviors, and form 
+online communities that facilitate radicalization. When trying to theorize the process of online and offline dynamics, 
+the article acknowledges the ambiguity of the term online radicalization and suggests that scholars should move beyond 
+the narrow focus on online radicalization. It recommends considering the wider information environment while avoiding 
+theories that exclusively explain how online radicalization works and advocates for holistic theories that consider 
+individuals' predispositions, stressors, engagement with their environment, and systemic factors.
+"""
+
+police_text = """
+In correlation to this, the text by the International Association of Chiefs Police, entitled 
+“Online Radicalization to Violent Extremism,” briefly touches on the topic highlighting the process by which individuals 
+are exposed to extremist ideologies and gradually move from mainstream beliefs toward more extreme views. Online media, 
+particularly social networks like Facebook, Twitter, and YouTube, play a significant role in this process. The text 
+emphasizes that the factors influencing radicalization vary from person to person, and the process itself can be dynamic, 
+with individuals moving back and forth between stages. This also leads into different perspectives.
+"""
+
+# Displaying the content using Streamlit
+st.write(article_intro)
+st.write(article_main)
+st.write(police_text)
+
+st.markdown("Firstly, the social perspective and the induvial focus; The concept of radicalization usually emphasizes "
+            "the psychological factors, and the effect online platforms and social media have on the induvial "
+            "constantly this rhetoric of construed violent beliefs. Group dynamics also play an important in this "
+            "first perspective because not only can the group influence tend to pull individuals towards extremist "
+            "ideologies, but the engagement also associated with the mind of interaction and sense of belonging "
+            "become an intense pull factor towards those ideas")
+st.write("The psychological perspective such as individual vulnerabilities due to grievances and confirmation bias, "
+         "wherein individuals seek information aligning with their existing beliefs, contributes to the reinforcement "
+         "of radical ideologies act as factors for this perspective however, the line can become misconstrued for "
+         "example, the horrific crimes Anders Breivik committed, the question of mental health became a key topic in "
+         "his trail, although sentenced to 21 years, the question still remains of mental health as strategic factor "
+         "for mitigated legal consequences or for public sympathy.")
+st.write("Another less widely looked at perspective is the political perspective where an individual's ideologies are "
+         "fueled by key factors including but not limited, to their socio-political and economic conditions marked by "
+         "discrimination, inequality, and the use of propaganda. ")
