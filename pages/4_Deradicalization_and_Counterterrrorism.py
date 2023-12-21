cead26b4663e191feeb6ee578474a8ed6294ec18
Aya <147270454+ayaarias21@users.noreply.github.com>
2023-12-20 20:18:38 -0500

    Create 4_Deradicalization_and_Counterterrrorism.py

---
+++ pages/4_Deradicalization_and_Counterterrrorism.py
@@ -0,0 +1,45 @@
+# Import necessary libraries
+import streamlit as st
+#from streamlit_option_menu import option_menu
+import pandas as pd  # read csv, df manipulation
+st.set_page_config(page_title="Bytes and Bombs", page_icon="🚨", layout="centered")
+st.markdown("<h2 style='color: #990000;'>Deradicalization and Counter Terrorism </h2>",
+            unsafe_allow_html=True)
+st.write("In an interview with Richard A. Clarke, a former senior White House advisor, the discussion revolves around "
+         "the risks of cyber war and cyber terrorism and ways to address them. Clarke emphasizes that while "
+         "cyber-attacks with limited objectives have occurred, a full-scale cyber war is yet to happen. He believes "
+         "that U.S. leaders understand the threat but haven't taken sufficient action, suggesting that a large-scale "
+         "cyber-attack may be necessary to alert the government causing legislative action. ")
+st.write('According to Koehler, D, “Understanding Deradicalization” book, in the introduction in chapter 1, '
+         'deradicalization is defined as a process of individual or collective cognitive change from criminal, '
+         'radical, or extremist identities to a non-criminal or moderate psychological state. It involves turning '
+         'away from a position of endorsing and using violence towards abstinence from violent means and/or '
+         'attitudes. The process of deradicalization is differentiated from "disengagement," which only denotes a '
+         'behavioral role change from offending to non-offending, The text also acknowledges the lack of conceptual '
+         'clarity in the emerging topic on deradicalization and the variety of terms used globally to describe a '
+         'similar process of moving away from violent ideologies or actions.  Additionally, in the text “The Cyber '
+         'Defense Review – Countering the Cyber Threat”, it calls for the development of consensus, involving the '
+         'unified effort to address the cybersecurity challenge by building consensus amongst public and private '
+         'sectors. The development of public-private, cross-firm, and cross-industry liaison networks.  would be used '
+         'to minimize localized thinking in threat response and enhance cooperation among firms and governments to '
+         'address challenges that span different sectors. As well as this the addition of a tightly monitored '
+         'information and communications network would provide rapidly declassified and anonymized threat indicators, '
+         'facilitating the quick detection of emerging attacks, and enabling attribution. With these measures this '
+         'will create a more team-based approach to this threat. ')
+st.write('Moving away from the call to state effort counter of terrorism as countries are starting to recognize the '
+         'need for specialized units to counter cyber threats, in the text “Countering Violent Extremism: A '
+         'Peacebuilding Perspective”, by Goergia Holmer, the author understands the evolving nature of violent '
+         'extremism, and aims for intersection between the counter violent extremism field and peacebuilding of '
+         'people. While CVE primarily operates within the international and national security policymaking community '
+         'for rules and regulations. peacebuilders also address the issue of violent extremism through conflict '
+         'prevention efforts. The two fields increasingly intersect, although their approaches and practices may '
+         'differ. With the idea of Peacebuilding contributions such as "do-no-harm" methodology, ensuring that '
+         'programs do not have unintended negative consequences, building local capacity and engaging existing '
+         'mechanisms, networks, and practices to contribute to Counter Violent Extremist efforts. An advocation for a '
+         'more coordinated and inclusive approach that leverages the strengths of both to address the complex and '
+         'evolving challenges of violent extremism. The article concludes with recommendations for holistic '
+         'de-radicalization, emphasizing re-education, vocational training, financial support, and the creation of a '
+         'conducive environment for reintegration. ')
+# More information
+
+#st.write("[Learn More >](https://...)") #how to include a link
